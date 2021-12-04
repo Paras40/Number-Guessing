0a74d4b44b70ce6c042dafbaa025e5b2cd2d87dc
Paras Tewari <91774904+Paras40@users.noreply.github.com>
2021-12-04 20:28:04 +0530

    Update guessit.py

--- guessit.py
+++ guessit.py
@@ -1,13 +1,20 @@
 import random
 attempts_list = []
 def show_score():
-    if len(attempts_list) <= 0:
-        print("There is currently no high score, it's yours for the taking!")
-    else:
-        print("The current high score is {} attempts".format(min(attempts_list)))
+    with open("hiscore.txt", "r") as f:
+        hiscore = int(f.read())   
+        print(f"The current high score is {hiscore} attempts")
+def update_score():
+    with open("hiscore.txt", "r") as f:
+        hiscore = int(f.read())
+        for score in attempts_list:
+            if(score<hiscore):
+                with open("hiscore.txt", "w") as f:
+                    f.write(str(score))
+                    print("==========You have just broken the high score!===============")
 def start_game():
     random_number = int(random.randint(1, 10))
-    print("Hello traveler! Welcome to the game of guesses!")
+    print("Hello! Welcome to the game of guesses!")
     player_name = input("What is your name? ").title().strip()
     wanna_play = input("Hi, {}, would you like to play the guessing game? (Enter Yes/No): ".format(player_name))
     attempts = 0
@@ -22,12 +29,13 @@ def start_game():
                 attempts += 1
                 attempts_list.append(attempts)
                 print("It took you {} attempts".format(attempts))
+                update_score()
                 play_again = input("Would you like to play again? (Enter Yes/No): ")
                 attempts = 0
                 show_score()
                 random_number = int(random.randint(1, 10))
                 if play_again.lower() == "no":
-                    print("That's cool, have a good one!")
+                    print("That's cool, have a good day!")
                     break
             elif int(guess) > random_number:
                 print("It's higher")
@@ -39,6 +47,7 @@ def start_game():
             print("Oh no!, that is not a valid value. Try again...")
             print("({})".format(err))
     else:
-        print("That's cool, have a good one!")
+        print("That's cool, have a good day!")
+
 if __name__ == '__main__':
     start_game()
