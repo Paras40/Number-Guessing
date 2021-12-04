3b3a0826d2b3d99c86e0671cac4c6300c5fed436
Paras Tewari <91774904+Paras40@users.noreply.github.com>
2021-12-04 10:13:38 +0530

    Update and rename Email_slicer.py to guessit.py

--- Email_slicer.py
+++
@@ -1,7 +0,0 @@
-email = input("Write your email here : ").strip()
-
-#index is a in-built function
-username = email[:email.index("@")]
-domain = email[email.index("@")+1:]
-
-print(f"Your username is {username} and domain is {domain}")
